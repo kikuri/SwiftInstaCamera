297360b8b9fa66eadeb36fdf50a3cb62bb471c3f
kikurii <wataru.kikuchi@notari.co.jp>
2017-01-04 15:34:02 +0900

    Initial Commit

---
+++ SwiftInstaCamera1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftInstaCamera1
+//
+//  Created by kikuchi wataru on 2017/01/04.
+//  Copyright © 2017年 kikuchi wataru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
